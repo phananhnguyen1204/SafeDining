383290607621ca3e279d54922229c4a5843707fe
Phan Anh <phananh12042003@gmail.com>
2024-02-04 13:29:04 -0500

    cart functionality

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
   path('', views.marketplace, name='marketplace'),
-  path('<slug:vendor_slug>/', views.vendor_detail, name='vendor_detail')
+  path('<slug:vendor_slug>/', views.vendor_detail, name='vendor_detail'),
+  
+  #Add to cart
+  path('add_to_cart/<int:food_id>/', views.add_to_cart,name='add_to_cart'),
+  #Decrease cart
+  path('decrease_cart/<int:food_id>/', views.decrease_cart,name='decrease_cart'),
 ]

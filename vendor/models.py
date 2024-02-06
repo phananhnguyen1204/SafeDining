5dfab3a60108e37c1da3f549037f9f386271603b
Phan Anh <phananh12042003@gmail.com>
2024-02-06 08:53:09 -0500

    business opening hours

--- vendor/models.py
+++ vendor/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from accounts.models import User, UserProfile
 from accounts.utils import send_notification
-from datetime import time
+from datetime import time, date, datetime
 
 # Create your models here.
 class Vendor(models.Model):
@@ -16,6 +16,25 @@ class Vendor(models.Model):
   
   def __str__(self):
     return self.vendor_name
+  
+  def is_open(self):
+    # Check current day's opening hours.
+    today_date = date.today()
+    today = today_date.isoweekday()
+    current_opening_hours = OpeningHour.objects.filter(vendor=self,day=today)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    is_open = None
+    print(current_time)
+    for i in current_opening_hours:
+        start = str(datetime.strptime(i.from_hour, "%I:%M %p").time())
+        end = str(datetime.strptime(i.to_hour, "%I:%M %p").time())
+        if current_time > start and current_time < end:
+            is_open = True
+        else:
+            is_open = False
+    return is_open
 
   def save(self, *args, **kwargs):
     if self.pk is not None:
@@ -39,13 +58,13 @@ class Vendor(models.Model):
   
 
 DAYS = [
-  (1, ("MONDAY")),  
-  (2, ("Tuesday")),  
-  (3, ("Wednesday")),  
-  (4, ("Thursday")),  
-  (5, ("Friday")),  
-  (6, ("Saturday")),  
-  (7, ("Sunday")),  
+  (1, "Monday"),  
+  (2, "Tuesday"),  
+  (3, "Wednesday"),  
+  (4, "Thursday"),  
+  (5, "Friday"),  
+  (6, "Saturday"),  
+  (7, "Sunday"),  
 
 ]
 
@@ -53,14 +72,16 @@ HOUR_OF_DAY_24 = [(time(h,m).strftime('%I:%M %p'), time(h,m).strftime('%I:%M %p'
 
 class OpeningHour(models.Model):
   vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-  day = models.IntegerField(choices =DAYS)
+  day = models.IntegerField(choices = DAYS)
   from_hour = models.CharField(choices = HOUR_OF_DAY_24, max_length=10,blank=True)
   to_hour = models.CharField(choices = HOUR_OF_DAY_24, max_length=10,blank=True)
   is_closed = models.BooleanField(default=False)
   
   class Meta:
-    ordering = ('day', 'from_hour')
-    unique_together = ('day', 'from_hour', 'to_hour')
+    ordering = ('day', '-from_hour')
+    unique_together = ('vendor','day', 'from_hour', 'to_hour')
+  def __str__(self):
+    return self.get_day_display()
   
     
   
